bdf793a2ed9de6ef2706f75670128010e63d49fa
nate <nate@pop-os.localdomain>
2020-09-19 01:25:11 -0500

    more cleanup

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ WORKDIR /Build
 # debug first
 RUN cd Debug && dotnet build -c Release
 RUN cd Subrace && dotnet build -c Release
+RUN cd Tracking && dotnet build -c Release
 # ...
 
 # Build the final NWN server image (Version: 8193.14)
@@ -15,8 +16,9 @@ LABEL maintainer "urothis@gmail.com"
 # debug
 COPY --from=build /Build/Debug/bin/Release/Plugins/ /nwn/Dotnet/Plugins
 COPY --from=build /Build/Subrace/bin/Release/Plugins/ /nwn/Dotnet/Plugins
+COPY --from=build /Build/Tracking/bin/Release/Plugins/ /nwn/Dotnet/Plugins
 # ...
 
 # install the unzip package so we can grab the latest managed binaries
 RUN apt update && apt upgrade -y && apt install unzip
-RUN cd /nwn/Dotnet && wget "https://github.com/nwn-dotnet/NWN.Managed/releases/download/v8193.14.23/NWN.Managed.zip" -O temp.zip && unzip temp.zip && rm temp.zip && ls ./Plugins/debug
+RUN cd /nwn/Dotnet && wget "https://github.com/nwn-dotnet/NWN.Managed/releases/download/v8193.14.23/NWN.Managed.zip" -O temp.zip && unzip temp.zip && rm temp.zip
---
+++ src/Services/Tracking/Tracking.cs
@@ -0,0 +1,44 @@
+using System;
+using NWN.API;
+using NWN.API.Events;
+using NWN.Services;
+
+namespace Bloodstone {
+    // [ServiceBinding] indicates that this class will be created during server startup.
+    [ServiceBinding(typeof(Tracking))]
+    public class Tracking {
+        // Called at startup. NWN.Managed resolves EventService for us.
+        public Tracking(EventService eventService) {
+            eventService.Subscribe<NwModule, ModuleEvents.OnClientEnter>(NwModule.Instance, OnClientEnter);
+            eventService.Subscribe<NwModule, ModuleEvents.OnPlayerLevelUp>(NwModule.Instance, OnPlayerLevelUp);
+            eventService.Subscribe<NwModule, ModuleEvents.OnPlayerRespawn>(NwModule.Instance, OnPlayerRespawn);
+            eventService.Subscribe<NwModule, ModuleEvents.OnPlayerRest>(NwModule.Instance, OnPlayerRest);
+            eventService.Subscribe<NwModule, ModuleEvents.OnClientLeave>(NwModule.Instance, OnClientLeave);
+            eventService.Subscribe<NwModule, ModuleEvents.OnPlayerDying>(NwModule.Instance, OnPlayerDying);
+        }
+
+        private void OnClientEnter(ModuleEvents.OnClientEnter onClientEnter) {
+            Console.WriteLine(onClientEnter.Player.Name);
+        }
+
+        private void OnPlayerLevelUp(ModuleEvents.OnPlayerLevelUp onPlayerLevelUp) {
+            Console.WriteLine(onPlayerLevelUp.Player.Name);
+        }
+
+        private void OnPlayerRespawn(ModuleEvents.OnPlayerRespawn onPlayerRespawn) {
+            Console.WriteLine(onPlayerRespawn.Player.Name);
+        }
+
+        private void OnPlayerRest(ModuleEvents.OnPlayerRest onPlayerRest) {
+            Console.WriteLine(onPlayerRest.Player.Name);
+        }
+
+        private void OnClientLeave(ModuleEvents.OnClientLeave onClientLeave) {
+            Console.WriteLine(onClientLeave.Player.Name);
+        }
+
+        private void OnPlayerDying(ModuleEvents.OnPlayerDying onPlayerDying) {
+            Console.WriteLine(onPlayerDying.Player.Name);
+        }
+    }
+}
